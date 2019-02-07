64de922af8900b46919e1916ef21209f61366191
gpalozzi <gpalozzi@gmail.com>
2019-02-07 17:25:05 +0000

    wip

--- client/src/components/addFund.vue
+++ client/src/components/addFund.vue
@@ -23,12 +23,14 @@
             @click="create"
           >
             <v-icon class="mr-2">fa-plus</v-icon>
-            Create (0.0015 PPT / ${{0.0015 * ethUsdPrice}})
+            Create (0.0015 PPT / ${{0.0015 * prices.ETH}})
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
-      <v-stepper v-model="el">
+      <v-stepper v-model="el" alt-labels>
         <v-stepper-header>
+          <v-divider></v-divider>
+
           <v-stepper-step :complete="el > 1" step="1">Fund details</v-stepper-step>
 
           <v-divider></v-divider>
@@ -37,12 +39,11 @@
 
           <v-divider></v-divider>
 
-          <v-stepper-step step="3">Democracy</v-stepper-step>
+          <!-- <v-stepper-step step="3">Democracy</v-stepper-step> -->
         </v-stepper-header>
 
         <v-stepper-items>
           <v-stepper-content step="1">
-            <!-- <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card> -->
             <v-card-text>
               <v-container grid-list-xl>
                 <v-layout wrap>
@@ -75,20 +76,47 @@
           </v-stepper-content>
 
           <v-stepper-content step="2">
-            <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+            <v-layout row>
+              <v-flex xs12 sm6 offset-sm3>
+                <v-list two-line>
+                  <template v-for="(item, index) in items">
+                    <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
 
-            <v-btn color="primary" @click="el = 3">Continue</v-btn>
+                    <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
+
+                    <v-list-tile v-else :key="item.token_symbol" avatar @click>
+                      <v-list-tile-avatar>
+                        <img :src="item.avatar">
+                      </v-list-tile-avatar>
+
+                      <v-list-tile-content>
+                        <v-list-tile-title v-html="item.token_symbol"></v-list-tile-title>
+                        <v-list-tile-sub-title v-html="item.description"></v-list-tile-sub-title>
+                      </v-list-tile-content>
+                      <v-text-field
+                        v-model="percentage"
+                        :rules="percentageRules"
+                        type="number"
+                        suffix="%"
+                        @onChange="tokens[token_symbol] = percentage"
+                      ></v-text-field>
+                    </v-list-tile>
+                  </template>
+                </v-list>
+              </v-flex>
+            </v-layout>
+            <v-btn color="primary" @click="el = 3">Create</v-btn>
 
             <v-btn flat>Cancel</v-btn>
           </v-stepper-content>
 
-          <v-stepper-content step="3">
+          <!-- <v-stepper-content step="3">
             <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
 
             <v-btn color="primary" @click="el = 1">Continue</v-btn>
 
             <v-btn flat>Cancel</v-btn>
-          </v-stepper-content>
+          </v-stepper-content>-->
         </v-stepper-items>
       </v-stepper>
     </v-form>
@@ -107,6 +135,8 @@ const nameLength = (v: string) =>
 const symbolRequired = (v: string) => !!v || "Fund symbol is required";
 const symbolLength = (v: string) =>
   (v && v.length <= 5) || "Fund symbol must be less than 5 characters";
+const percentageMaxValue = (v: number) =>
+  (v && v <= 100) || "The maximum percentage is 100%";
 
 @Component
 export default class AddFund extends Vue {
@@ -115,15 +145,44 @@ export default class AddFund extends Vue {
   public nameRules = [nameRequired, nameLength];
   public symbol: string = "";
   public symbolRules = [symbolRequired, symbolLength];
+  public percentageRules = [percentageMaxValue];
   public loading = false;
-  public el: number = 1;
+  public el: number = 0;
+  public tokens = {};
+  public items = [
+    { header: "Available Tokens" },
+    {
+      avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+      token_symbol: "AAPL",
+      description:
+        "<span class='text--primary'>$3.022</span> &mdash; Apple stocks tokenApple stocks tokenApple stocks tokenApple stocks tokenApple stocks token"
+    },
+    { divider: true, inset: true },
+    {
+      avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+      token_symbol: "DAS",
+      description:
+        "<span class='text--primary'>$5.022</span> &mdash; Das tokenApple stocks tokenApple stocks tokenApple stocks tokenApple stocks token"
+    },
+    { divider: true, inset: true },
+    {
+      avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+      token_symbol: "SNT",
+      description:
+        "<span class='text--primary'>$1.022</span> &mdash; Status tokenApple stocks tokenApple stocks tokenApple stocks tokenApple stocks token"
+    }
+  ];
 
   @Action("create", { namespace: "fund" }) private createFund!: (
     fund: Fund
   ) => void;
-  @State("ethUsdPrice") private ethUsdPrice!: number;
+  @State("prices") private prices!: any;
 
   public async create() {
+    const fund = {
+      name: this.name,
+      symbol: this.symbol
+    };
   }
 }
 </script>
